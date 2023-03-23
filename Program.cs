260c5d5019078958595ea2dddba74844742863ed
Alycat <Arc183@student.aru.ac.uk>
2023-03-23 09:52:07 +0000

    Changed Internal to External Jason, Added Readkey

--- Program.cs
+++ Program.cs
@@ -3,6 +3,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+        Console.ReadKey(true);
     }
 }
